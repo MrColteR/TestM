e99cd4c10476aa6f83ea4068ff11eb7cc37f498d
MrColteR <MrColteR.com>
2021-11-24 15:20:37 +0300

    Добавьте файлы проекта.

---
+++ TestM/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestM.ViewModels.Base
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop = "")
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ TestM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestM.ViewModels.Base;
+
+namespace TestM.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+
+    }
+}
